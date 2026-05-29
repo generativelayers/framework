7cec554545881cea48831ce30cd375a8fb3563c7
Dimitrios Kyriakidis <93831628+kyriakidisdimitrios@users.noreply.github.com>
2026-05-29 13:36:26 +0300

    Add initial core class

---
+++ src/main/java/org/generativelayers/core/GenerativeLayers.java
@@ -0,0 +1,10 @@
+package org.generativelayers.core;
+
+public final class GenerativeLayers {
+    private GenerativeLayers() {
+    }
+
+    public static String name() {
+        return "Generative Layers";
+    }
+}
