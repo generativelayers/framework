3820bbfd4e32b66e5409c2be265eece94f3ff1f9
Dimitrios Kyriakidis <93831628+kyriakidisdimitrios@users.noreply.github.com>
2026-06-08 21:38:14 +0300

    Add short public Jason action aliases

---
+++ core/src/main/java/gl/jason/valid.java
@@ -0,0 +1,12 @@
+package gl.jason;
+
+/**
+ * Short public Jason internal-action alias.
+ *
+ * <p>Normal Jason usage:
+ * <pre>
+ * gl.jason.valid(ResultId, IsValid);
+ * </pre>
+ */
+public class valid extends gl.adapter.jason.actions.valid {
+}
