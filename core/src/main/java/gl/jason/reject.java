7cb3388e2660a4989c5138cfed39cd8aff9fe3d9
Dimitrios Kyriakidis <93831628+kyriakidisdimitrios@users.noreply.github.com>
2026-06-08 21:37:35 +0300

    Add short public Jason action aliases

---
+++ core/src/main/java/gl/jason/reject.java
@@ -0,0 +1,12 @@
+package gl.jason;
+
+/**
+ * Short public Jason internal-action alias.
+ *
+ * <p>Normal Jason usage:
+ * <pre>
+ * gl.jason.reject(CandidateId);
+ * </pre>
+ */
+public class reject extends gl.adapter.jason.actions.reject {
+}
