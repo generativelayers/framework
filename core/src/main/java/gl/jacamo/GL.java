ba0a64acdba0cac00bfd12702213d0d00a8d1756
Dimitrios Kyriakidis <93831628+kyriakidisdimitrios@users.noreply.github.com>
2026-06-08 21:29:53 +0300

    Add short public platform aliases

---
+++ core/src/main/java/gl/jacamo/GL.java
@@ -0,0 +1,18 @@
+package gl.jacamo;
+
+/**
+ * Short public JaCaMo/CArtAgO artifact alias for the Generative Layers framework.
+ *
+ * <p>Normal JaCaMo artifact creation:
+ * <pre>
+ * makeArtifact("gl", "gl.jacamo.GL", [], Id);
+ * focus(Id);
+ * </pre>
+ *
+ * <p>This is the public, concise form of the full implementation class:
+ * <pre>
+ * makeArtifact("gl", "gl.adapter.jacamo.JaCaMoAdapter", [], Id);
+ * </pre>
+ */
+public class GL extends gl.adapter.jacamo.JaCaMoAdapter {
+}
