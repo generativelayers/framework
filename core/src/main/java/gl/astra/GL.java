1d495fcb2085456b77328733e6860e6b0fda0f71
Dimitrios Kyriakidis <93831628+kyriakidisdimitrios@users.noreply.github.com>
2026-06-08 21:29:23 +0300

    Add short public platform aliases

---
+++ core/src/main/java/gl/astra/GL.java
@@ -0,0 +1,17 @@
+package gl.astra;
+
+/**
+ * Short public ASTRA module alias for the Generative Layers framework.
+ *
+ * <p>Normal ASTRA import:
+ * <pre>
+ * module gl.astra.GL gl;
+ * </pre>
+ *
+ * <p>This is the public, concise form of the full implementation import:
+ * <pre>
+ * module gl.adapter.astra.AstraAdapter gl;
+ * </pre>
+ */
+public class GL extends gl.adapter.astra.AstraAdapter {
+}
